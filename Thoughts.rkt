59592543397ec3fe632c95467049b7c56a538061
JY <jy1486@nyu.edu>
2021-03-10 00:28:40 -0500

    Update Thoughts.rkt

--- Thoughts.rkt
+++ Thoughts.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(lambda (x) (+ x 2))
+(lambda (x) (+ x 2)) ;lambda notation 
 ((lambda (x) (+ x 2))2)
 
 (define (for initialCount FinalCount State StateTransition)
@@ -8,4 +8,22 @@
       State
       (for (+ initialCount 1) FinalCount (StateTransition State) StateTransition)))
 
-(for 0 5 3 (lambda (x) (+ x 2)))
+(for 0 5 77 (lambda (x) (+ x 2))) ;for loop [0,1,2,3,4], add 2 to 77 five times
+
+
+(define (superfib n)
+  (helpfib 0 1 1 0 n))
+(define (helpfib a b p q n)
+  (cond ((= n 0) b)
+        ((even? n) (helpfib a
+                            b
+                            (+ (expt p 2) (* 2 p q))
+                            (+ (expt p 2) (expt q 2))
+                            (/ n 2)))
+        ((odd? n) (helpfib (+ (* p b) (* q a))
+                           (+ (* b (+ p q)) (* p a))
+                           p
+                           q
+                           (- n 1) ))))
+
+(superfib 9)
